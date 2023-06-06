ad891f0512baf7ff6d30479562b62fc9976794ef
koki yoda <134734832+kokiyoda@users.noreply.github.com>
2023-06-07 03:04:02 +0900

    Add files via upload

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,9 @@
 package org.example;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
 import java.util.stream.Stream;
 
 public class Main {
